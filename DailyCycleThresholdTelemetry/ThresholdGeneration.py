93c17a4388f1a44ab0829c794eb592c6341203e3
Linnea <linnea.gustavsen@gmail.com>
2023-02-07 15:11:07 +0100

    Tested different thresholds also on the daily cycle

--- DailyCycleThresholdTelemetry/ThresholdGeneration.py
+++ DailyCycleThresholdTelemetry/ThresholdGeneration.py
@@ -63,3 +63,10 @@ def thresholdGeneration(systemId, if_name, field):
     json_file_mean_var.close()
 
 thresholdGeneration("trd-gw", "xe-0/1/0", "egress_stats__if_1sec_pkts")
+thresholdGeneration("trd-gw", "xe-0/1/0", "ingress_stats__if_1sec_pkts")
+thresholdGeneration("trd-gw", "et-11/0/0", "ingress_stats__if_1sec_pkts")
+thresholdGeneration("trd-gw", "et-11/0/0", "egress_stats__if_1sec_pkts")
+thresholdGeneration("hmg9-gw1", "et-0/1/4", "ingress_stats__if_1sec_pkts")
+thresholdGeneration("hmg9-gw1", "et-0/1/4", "egress_stats__if_1sec_pkts")
+thresholdGeneration("hovedbygget-gw", "et-11/0/2", "egress_stats__if_1sec_pkts")
+thresholdGeneration("hovedbygget-gw", "et-11/0/2", "ingress_stats__if_1sec_pkts")
