f035dbaa890aa8956eb7444c6b8ca4f734de94f8
Linnea <linnea.gustavsen@gmail.com>
2023-02-22 18:02:56 +0100

    Made training dataset

---
+++ RandomForest/GetData.py
@@ -0,0 +1,91 @@
+from influxdb_client import InfluxDBClient
+
+import numpy as np
+from Distributions import *
+from GeneralizedEntropy import *
+import math
+from datetime import datetime, timedelta
+import pandas as pd
+'''
+start: datetime
+stop: datetime
+systemId: string
+if_name: string
+field: string
+'''
+def getData(start, stop, systemId, if_name, fields):
+    client = InfluxDBClient(url="http://localhost:8086", token="XIXjEYH2EUd8fewS0niwHcdif20ytyhNR3dqPYppD0S8LQeA7CnICVVnlke6H3kmN0cvTVoINmXqz1aCbCxL6A==", org="4bad65ca5da036f7", timeout=100000)
+
+    query_api = client.query_api()
+    columns = ["_time"]
+
+    query = 'from(bucket: "skogul/1mnd")\
+                |> range(start: ' + start + ', stop: ' + stop + ')\
+                |> filter(fn: (r) => r["systemId"] == "' + systemId + '")\
+                |> filter(fn: (r) => r["if_name"] == "' + if_name + '")\
+                |> filter(fn: (r) => '
+    for i in range(len(fields)):
+        query += 'r["_field"] == "' + fields[i] + '"'
+        columns.append(fields[i])
+        if len(fields) != 1 and i != len(fields)-1:
+            query += ' or '
+        
+    query += ')\
+    |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")\
+    |> keep(columns: ' + str(columns).replace("'", '"') + ')'
+
+    #Make a data frame from the output of the query
+    df = query_api.query_data_frame(query=query)
+
+    if not df.empty:
+        df = df.drop(columns=['result', 'table'])
+
+    return df
+
+
+def getEntropyData(systemId,if_name, start, stop):
+    intervalTime = (stop - start).total_seconds()/60
+
+    packetSizeArray = []
+    packetSizeRateArray = []
+    timeArray = []
+    startTime = start
+
+    for i in range(math.ceil(intervalTime)):
+        stopTime = startTime + timedelta(minutes = 5)
+        #Get data for a specified time interval
+        df = getData(startTime.strftime("%Y-%m-%dT%H:%M:%SZ"), stopTime.strftime("%Y-%m-%dT%H:%M:%SZ"),systemId, if_name, ["egress_stats__if_1sec_octets", "egress_stats__if_1sec_pkts"])
+        
+        #If there is not enough datapoints the minute is skipped
+        if df.empty:
+            continue
+        egressBytes = df["egress_stats__if_1sec_octets"].to_numpy()
+        egressPackets = df["egress_stats__if_1sec_pkts"].to_numpy()
+        
+        
+
+        
+
+        timeArray.append(startTime.strftime("%Y-%m-%d %H:%M"))
+
+        #Find the probability distribution based on how big the packets are this time interval
+        PiPS,nd = packetSizeDistribution(egressBytes, egressPackets)
+        #Calculate the generalized entropy of this distribution
+        entropyPacketSize = generalizedEntropy(10, PiPS)
+        packetSizeArray.append(entropyPacketSize)
+
+        #Calculate the generalized entropy rate of this distribution
+        entropyRatePacketSize = entropyPacketSize/nd
+        packetSizeRateArray.append(entropyRatePacketSize)
+
+        #Push the start time by the specified frequency
+        startTime = startTime + timedelta(minutes = 1)
+
+    entropy = pd.DataFrame(
+    {"_time": timeArray,
+     "entropy_packet_size": packetSizeArray,
+     "entropy_rate_packet_size": packetSizeRateArray
+    })
+    entropy.to_pickle("RandomForest/entropy.pkl")
+
+    return packetSizeArray, packetSizeRateArray, timeArray
