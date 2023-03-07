ee5548798d67daa2ac76cd1b18cc28d5b0b551fb
Linnea <linnea.gustavsen@gmail.com>
2023-03-07 19:06:00 +0100

    small fix

--- VictimContainer/scripts/capture.sh
+++ VictimContainer/scripts/capture.sh
@@ -2,16 +2,16 @@
 #Import variables from file
 source variables.sh
 
-for i in {1HelperFunctions.120}
+for i in {1..120}
 do
     file_name="/home/wiresharkTraces/Trace"
     current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-    suffix=".pcap"
+    suffix="pcap"
     new_fileName=$file_name.$current_time.$suffix
     #Start Wireshark capture
     tshark -i $interface -w "$new_fileName" -F pcap & pid_tshark=$!
     #Wait for next attack
     sleep 60
     #Stop the Wireshark capture
-    kill "$pid_tshark"
+    kill $pid_tshark
 done
