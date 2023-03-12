cd3e8f4962e5dfb47e6b27f1c1508070494bd6bc
Linnea <linnea.gustavsen@gmail.com>
2023-03-12 16:21:45 +0100

    Store Entropy historic in this branch

--- NetFlow/HistoricEntropy/CompareHistoricToInterval.py
+++ NetFlow/HistoricEntropy/CompareHistoricToInterval.py
@@ -4,12 +4,12 @@ from HelperFunctions.MakePlot import *
 df_historic= pd.read_csv('NetFlow/HistoricEntropy/Calculations/Metrics.attack.08.03.csv')
 df_interval= pd.read_csv('NetFlow/Entropy/Calculations/Metrics.attack.08.03.csv')
 
-'''makePlot(df_historic['srcEntropy'], df_historic['Time'], "historicSrcEntropy", "NetFlow/HistoricEntropy/HelperFunctions/Plots/")
-makePlot(df_interval['srcEntropy'], df_interval['Time'], "intervalSrcEntropy", "NetFlow/HistoricEntropy/HelperFunctions/Plots/")
-makePlot(df_historic['dstEntropy'], df_historic['Time'], "historicDstEntropy", "NetFlow/HistoricEntropy/HelperFunctions/Plots/")
-makePlot(df_interval['dstEntropy'], df_interval['Time'], "intervalDstEntropy", "NetFlow/HistoricEntropy/HelperFunctions/Plots/")
-makePlot(df_historic['flowEntropy'], df_historic['Time'], "historicFlowEntropy", "NetFlow/HistoricEntropy/HelperFunctions/Plots/")
-makePlot(df_interval['flowEntropy'], df_interval['Time'], "intervalFlowEntropy", "NetFlow/HistoricEntropy/HelperFunctions/Plots/")'''
+'''makePlot(df_historic['srcEntropy'], df_historic['Time'], "historicSrcEntropy", "NetFlow/HistoricEntropy/Plots/")
+makePlot(df_interval['srcEntropy'], df_interval['Time'], "intervalSrcEntropy", "NetFlow/HistoricEntropy/Plots/")
+makePlot(df_historic['dstEntropy'], df_historic['Time'], "historicDstEntropy", "NetFlow/HistoricEntropy/Plots/")
+makePlot(df_interval['dstEntropy'], df_interval['Time'], "intervalDstEntropy", "NetFlow/HistoricEntropy/Plots/")
+makePlot(df_historic['flowEntropy'], df_historic['Time'], "historicFlowEntropy", "NetFlow/HistoricEntropy/Plots/")
+makePlot(df_interval['flowEntropy'], df_interval['Time'], "intervalFlowEntropy", "NetFlow/HistoricEntropy/Plots/")'''
 
 def makeMultiplePlots(valueArray1, valueArray2, timeArray, name, path):
     fig, ax = plt.subplots(1, 1, figsize=(20, 5))
@@ -21,6 +21,6 @@ def makeMultiplePlots(valueArray1, valueArray2, timeArray, name, path):
     fig.tight_layout()
     fig.savefig(path+name+".png")
 
-makeMultiplePlots(df_historic['srcEntropy'], df_interval['srcEntropy'], df_historic['Time'], "bothSrcEntropy", "NetFlow/HistoricEntropy/HelperFunctions/Plots/")
-makeMultiplePlots(df_historic['dstEntropy'], df_interval['dstEntropy'], df_historic['Time'], "bothDstEntropy", "NetFlow/HistoricEntropy/HelperFunctions/Plots/")
-makeMultiplePlots(df_historic['flowEntropy'], df_interval['flowEntropy'], df_historic['Time'], "bothFlowEntropy", "NetFlow/HistoricEntropy/HelperFunctions/Plots/")
+makeMultiplePlots(df_historic['srcEntropy'], df_interval['srcEntropy'], df_historic['Time'], "bothSrcEntropy", "NetFlow/HistoricEntropy/Plots/")
+makeMultiplePlots(df_historic['dstEntropy'], df_interval['dstEntropy'], df_historic['Time'], "bothDstEntropy", "NetFlow/HistoricEntropy/Plots/")
+makeMultiplePlots(df_historic['flowEntropy'], df_interval['flowEntropy'], df_historic['Time'], "bothFlowEntropy", "NetFlow/HistoricEntropy/Plots/")
