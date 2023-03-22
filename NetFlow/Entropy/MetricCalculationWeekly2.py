17d0aa41506047adbf5c0a0ad0d1685eb6348648
linneafgustavsen <linnea.gustavsen@gmail.com>
2023-03-22 11:42:20 +0100

    Fixed the metric calculation based on minute

--- NetFlow/Entropy/MetricCalculationWeekly2.py
+++ NetFlow/Entropy/MetricCalculationWeekly2.py
@@ -25,26 +25,42 @@ import json
 '''
 
 def metricCalculation(silkFile, start, stop, frequency, interval):
-    #Open file to write alerts to
-    json_file = open("NetFlow/Entropy/Schemas/RawValuesSchema.json", "r")
-    json_object_raw_sip = json.load(json_file)
-    json_file = open("NetFlow/Entropy/Schemas/RawValuesSchema.json", "r")
-    json_object_raw_sip_rate = json.load(json_file)
-    json_file = open("NetFlow/Entropy/Schemas/RawValuesSchema.json", "r")
-    json_object_raw_dip = json.load(json_file)
-    json_file = open("NetFlow/Entropy/Schemas/RawValuesSchema.json", "r")
-    json_object_raw_dip_rate = json.load(json_file)
-    json_file = open("NetFlow/Entropy/Schemas/RawValuesSchema.json", "r")
-    json_object_raw_f = json.load(json_file)
-    json_file = open("NetFlow/Entropy/Schemas/RawValuesSchema.json", "r")
-    json_object_raw_f_rate = json.load(json_file)
-    json_file = open("NetFlow/Entropy/Schemas/RawValuesSchema.json", "r")
-    json_object_raw_nf = json.load(json_file)
-    json_file = open("NetFlow/Entropy/Schemas/RawValuesSchema.json", "r")
-    json_object_raw_icmp_ratio = json.load(json_file)
-    json_file = open("NetFlow/Entropy/Schemas/RawValuesSchema.json", "r")
-    json_object_raw_icmp_packets = json.load(json_file)
-    json_file.close()
+    #Open file to write entropy to
+    json_file_raw_sip = open("NetFlow/Entropy/Calculations/sip."+ str(int(interval.total_seconds())) + ".json", "w")
+    json_object_raw_sip = json.load(json_file_raw_sip)
+    json_file_raw_sip.close()
+
+    json_file_raw_sip_rate = open("NetFlow/Entropy/Calculations/sip_rate."+ str(int(interval.total_seconds())) + ".json", "w")
+    json_object_raw_sip_rate = json.load(json_file_raw_sip_rate)
+    json_file_raw_sip_rate.close()
+
+    json_file_raw_dip = open("NetFlow/Entropy/Calculations/dip."+ str(int(interval.total_seconds())) + ".json", "w")
+    json_object_raw_dip = json.load(json_file_raw_dip)
+    json_file_raw_dip.close()
+
+    json_file_raw_dip_rate = open("NetFlow/Entropy/Calculations/dip_rate."+ str(int(interval.total_seconds())) + ".json", "w")
+    json_object_raw_dip_rate = json.load(json_file_raw_dip_rate)
+    json_file_raw_dip_rate.close()
+
+    json_file_raw_flow = open("NetFlow/Entropy/Calculations/flow."+ str(int(interval.total_seconds())) + ".json", "w")
+    json_object_raw_f = json.load(json_file_raw_flow)
+    json_file_raw_flow.close()
+
+    json_file_raw_f_rate = open("NetFlow/Entropy/Calculations/f_rate."+ str(int(interval.total_seconds())) + ".json", "w")
+    json_object_raw_f_rate = json.load(json_file_raw_f_rate)
+    json_file_raw_f_rate.close()
+
+    json_file_raw_nf = open("NetFlow/Entropy/Calculations/nf."+ str(int(interval.total_seconds())) + ".json", "w")
+    json_object_raw_nf = json.load(json_file_raw_nf)
+    json_file_raw_nf.close()
+
+    json_file_raw_icmp_ratio = open("NetFlow/Entropy/Calculations/icmp_ratio."+ str(int(interval.total_seconds())) + ".json", "w")
+    json_object_raw_icmp_ratio = json.load(json_file_raw_icmp_ratio)
+    json_file_raw_icmp_ratio.close()
+
+    json_file_raw_icmp_packets = open("NetFlow/Entropy/Calculations/icmp_packets."+ str(int(interval.total_seconds())) + ".json", "w")
+    json_object_raw_icmp_packets = json.load(json_file_raw_icmp_packets)
+    json_file_raw_icmp_packets.close()
 
 
 
@@ -151,8 +167,43 @@ def metricCalculation(silkFile, start, stop, frequency, interval):
 '''    
 metricCalculation("/home/linneafg/silk-data/RawDataFromFilter/one-day-2011-01-10_11-sorted.rw", "2011-01-10 00:00:00", "2011-01-11 00:00:00",timedelta(minutes = 1), timedelta(minutes = 5))'''
 silkFiles = ["/home/linneafg/silk-data/RawDataFromFilter/oslo-gw/week1.rw", "/home/linneafg/silk-data/RawDataFromFilter/oslo-gw/week2.rw", "/home/linneafg/silk-data/RawDataFromFilter/oslo-gw/week3.rw", "/home/linneafg/silk-data/RawDataFromFilter/oslo-gw/week4.rw", "/home/linneafg/silk-data/RawDataFromFilter/oslo-gw/week5.rw", "/home/linneafg/silk-data/RawDataFromFilter/oslo-gw/week6.rw", "/home/linneafg/silk-data/RawDataFromFilter/oslo-gw/week7.rw"]
-start="2010-12-27 00:00:00"
-stop="2011-01-03 00:00:00"
 
-metricCalculation(silkFiles[0], start, stop, timedelta(minutes = 1), timedelta(minutes = 5))
-metricCalculation(silkFiles[0], start, stop, timedelta(minutes = 1), timedelta(minutes = 10))
+start="2011-01-03 00:00:00"
+stop="2011-01-10 00:00:00"
+metricCalculation(silkFiles[1], start, stop, timedelta(minutes = 1), timedelta(minutes = 5))
+start="2011-01-10 00:00:00"
+stop="2011-01-17 00:00:00"
+metricCalculation(silkFiles[2], start, stop, timedelta(minutes = 1), timedelta(minutes = 5))
+start="2011-01-17 00:00:00"
+stop="2011-01-24 00:00:00"
+metricCalculation(silkFiles[3], start, stop, timedelta(minutes = 1), timedelta(minutes = 5))
+start="2011-01-24 00:00:00"
+stop="2011-01-31 00:00:00"
+metricCalculation(silkFiles[4], start, stop, timedelta(minutes = 1), timedelta(minutes = 5))
+start="2011-01-31 00:00:00"
+stop="2011-02-07 00:00:00"
+metricCalculation(silkFiles[5], start, stop, timedelta(minutes = 1), timedelta(minutes = 5))
+start="2011-02-07 00:00:00"
+stop="2011-02-14 00:00:00"
+metricCalculation(silkFiles[6], start, stop, timedelta(minutes = 1), timedelta(minutes = 5))
+
+
+
+start="2011-01-03 00:00:00"
+stop="2011-01-10 00:00:00"
+metricCalculation(silkFiles[1], start, stop, timedelta(minutes = 1), timedelta(minutes = 10))
+start="2011-01-10 00:00:00"
+stop="2011-01-17 00:00:00"
+metricCalculation(silkFiles[2], start, stop, timedelta(minutes = 1), timedelta(minutes = 10))
+start="2011-01-17 00:00:00"
+stop="2011-01-24 00:00:00"
+metricCalculation(silkFiles[3], start, stop, timedelta(minutes = 1), timedelta(minutes = 10))
+start="2011-01-24 00:00:00"
+stop="2011-01-31 00:00:00"
+metricCalculation(silkFiles[4], start, stop, timedelta(minutes = 1), timedelta(minutes = 10))
+start="2011-01-31 00:00:00"
+stop="2011-02-07 00:00:00"
+metricCalculation(silkFiles[5], start, stop, timedelta(minutes = 1), timedelta(minutes = 10))
+start="2011-02-07 00:00:00"
+stop="2011-02-14 00:00:00"
+metricCalculation(silkFiles[6], start, stop, timedelta(minutes = 1), timedelta(minutes = 10))
--- meeting.sh
+++ meeting.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 /bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/MetricCalculationWeekly.py
-
-/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/WeeklyFlowCalculation.py
-/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/WeeklyFlowCalculation2.py
-#/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/WeeklyFlowCalculation3.py
-/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/WeeklyFlowCalculation4.py
-/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/WeeklyFlowCalculation5.py
-/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/WeeklyFlowCalculation6.py
-#/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/WeeklyFlowCalculation7.py
-
-/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/WeeklySIPCalculation1.py
-#/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/WeeklySIPCalculation2.py
-#/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/WeeklySIPCalculation3.py
-#/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/WeeklySIPCalculation4.py
-#/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/WeeklySIPCalculation5.py
-#/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/WeeklySIPCalculation6.py
-#/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/WeeklySIPCalculation7.py
+/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/MetricCalculationWeekly2.py
