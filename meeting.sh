33fe61f39bd9451639f8f88791ea2620fa7c5224
linneafgustavsen <linnea.gustavsen@gmail.com>
2023-03-22 17:18:48 +0100

    Try again

--- meeting.sh
+++ meeting.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/MetricCalculationWeekly.py
 /bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/MetricCalculationWeekly2.py
+/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/MetricCalculationWeekly3.py
+/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/MetricCalculationWeekly4.py
+/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/MetricCalculationWeekly5.py
+/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/MetricCalculationWeekly6.py
+/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/MetricCalculationWeekly7.py
+
