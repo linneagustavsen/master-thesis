21658761408f52606db9efe6610228dcc5cae042
linneafgustavsen <linnea.gustavsen@gmail.com>
2023-04-10 22:47:49 +0200

    trying to make it more effective

--- meeting.sh
+++ meeting.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/MetricCalculationWeekly2.py
-/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/MetricCalculationWeekly3.py
-/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/MetricCalculationWeekly4.py
-/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/MetricCalculationWeekly5.py
-/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/MetricCalculationWeekly6.py
-/bin/python3 /home/linneafg/Code/master-thesis/NetFlow/Entropy/MetricCalculationWeekly7.py
+/bin/python3 /home/linneafg/Code/master-thesis/doMetricCalc.py &
 
