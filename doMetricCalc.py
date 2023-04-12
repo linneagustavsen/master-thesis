0f1f334f1b88a34d6be2d413790a0573822986aa
Linnea <linnea.gustavsen@gmail.com>
2023-04-12 10:15:28 +0200

    calculated the new weekly entropy

--- doMetricCalc.py
+++ doMetricCalc.py
@@ -9,7 +9,7 @@ from NetFlow.Entropy.Week7 import week7
 silkFiles = ["/home/linneafg/silk-data/RawDataFromFilter/oslo-gw/week1.rw", "/home/linneafg/silk-data/RawDataFromFilter/oslo-gw/week2.rw", "/home/linneafg/silk-data/RawDataFromFilter/oslo-gw/week3.rw", "/home/linneafg/silk-data/RawDataFromFilter/oslo-gw/week4.rw", "/home/linneafg/silk-data/RawDataFromFilter/oslo-gw/week5.rw", "/home/linneafg/silk-data/RawDataFromFilter/oslo-gw/week6.rw", "/home/linneafg/silk-data/RawDataFromFilter/oslo-gw/week7.rw"]
 start="2010-12-27 00:00:00"
 stop="2011-01-03 00:00:00"
-week1(silkFiles[1], start, stop, timedelta(minutes = 1), timedelta(minutes = 10))
+week1(silkFiles[0], start, stop, timedelta(minutes = 1), timedelta(minutes = 10))
 start="2011-01-03 00:00:00"
 stop="2011-01-10 00:00:00"
 week2(silkFiles[1], start, stop, timedelta(minutes = 1), timedelta(minutes = 10))
