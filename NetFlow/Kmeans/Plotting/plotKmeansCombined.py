8e9e306a536c61ea51eb157fa928f3dca8425d90
Linnea <linnea.gustavsen@gmail.com>
2023-05-10 20:57:41 +0200

    made correct plots for combined

--- NetFlow/Kmeans/Plotting/plotKmeansCombined.py
+++ NetFlow/Kmeans/Plotting/plotKmeansCombined.py
@@ -27,105 +27,120 @@ def plotKmeansFields(start, stop, interval, clusterFrequency, systemId, attackDa
     
     intervalTime = (stopTime - startTime).total_seconds()/clusterFrequency.total_seconds()
     timeAxis = []
-    packetsCluster0 = []
-    packetsCluster1 = []
-    sTimeCluster0 = []
-    sTimeCluster1 = []
+    packetsClusterAttack = []
+    packetsClusterNormal = []
+    sTimeClusterAttack = []
+    sTimeClusterNormal = []
+    maxValue = 0
     #Loop for every minute in a week
     for i in range(math.ceil(intervalTime)):
         stopTime = startTime + clusterFrequency
-  
 
-        cluster0 = pd.read_csv("Calculations0803/Kmeans/NetFlow/Combined.Cluster0."+ str(int(interval.total_seconds())) +"secInterval.attack."+str(attackDate)+ ".stopTime." + stopTime.strftime("%H.%M.%S")+ "."+ str(systemId)+ ".csv")
-        cluster1 = pd.read_csv("Calculations0803/Kmeans/NetFlow/Combined.Cluster1."+ str(int(interval.total_seconds())) +"secInterval.attack."+str(attackDate)+ ".stopTime." + stopTime.strftime("%H.%M.%S")+ "."+ str(systemId)+ ".csv")
-        sTime0 = pd.to_datetime(cluster0["sTime"])
-        sTime1 = pd.to_datetime(cluster1["sTime"])
-
-        packets0 = cluster0["packets"]
-        labels0 = cluster0["real_label"]
-        packets1 = cluster1["packets"]
-        labels1 = cluster1["real_label"]
+        clusterLabels = pd.read_csv("Calculations0803/Kmeans/NetFlow/Combined.ClusterLabelling."+ str(int(interval.total_seconds())) +"secInterval.attack."+str(attackDate)+ ".stopTime." + stopTime.strftime("%H.%M.%S")+ "."+ str(systemId)+ ".csv")
         
+        if clusterLabels["AttackCluster"][0] == 0:
+            attackCluster = pd.read_csv("Calculations0803/Kmeans/NetFlow/Combined.Cluster0."+ str(int(interval.total_seconds())) +"secInterval.attack."+str(attackDate)+ ".stopTime." + stopTime.strftime("%H.%M.%S")+ "."+ str(systemId)+ ".csv")
+            nonAttackCluster = pd.read_csv("Calculations0803/Kmeans/NetFlow/Combined.Cluster1."+ str(int(interval.total_seconds())) +"secInterval.attack."+str(attackDate)+ ".stopTime." + stopTime.strftime("%H.%M.%S")+ "."+ str(systemId)+ ".csv")
+        
+        elif clusterLabels["AttackCluster"][0] == 1:
+            attackCluster = pd.read_csv("Calculations0803/Kmeans/NetFlow/Combined.Cluster1."+ str(int(interval.total_seconds())) +"secInterval.attack."+str(attackDate)+ ".stopTime." + stopTime.strftime("%H.%M.%S")+ "."+ str(systemId)+ ".csv")
+            nonAttackCluster = pd.read_csv("Calculations0803/Kmeans/NetFlow/Combined.Cluster0."+ str(int(interval.total_seconds())) +"secInterval.attack."+str(attackDate)+ ".stopTime." + stopTime.strftime("%H.%M.%S")+ "."+ str(systemId)+ ".csv")
+        
+        print(clusterLabels["AttackCluster"])
+        sTimeAttack = pd.to_datetime(attackCluster["sTime"])
+        sTimeNormal = pd.to_datetime(nonAttackCluster["sTime"])
+
+        packetsAttack = attackCluster["packets"]
+        labelsAttack = attackCluster["real_label"]
+        packetsNormal = nonAttackCluster["packets"]
+        labelsNormal = nonAttackCluster["real_label"]
         
         #format = '%Y-%m-%dT%H:%M:%SZ'
-        eTime0 = pd.to_datetime(cluster0["eTime"])
-        eTime1 = pd.to_datetime(cluster1["eTime"])
+        eTimeAttack = pd.to_datetime(attackCluster["eTime"])
+        eTimeNormal = pd.to_datetime(nonAttackCluster["eTime"])
 
         lastInterval = pd.Interval(pd.Timestamp.now().replace(tzinfo=None), pd.Timestamp.now().replace(tzinfo=None), closed="both")
 
-        for i in range(len(labels0)):
-            if labels0[i] == 1:
-                if sTime0[i].replace(second=0).replace(tzinfo=None) in lastInterval and eTime0[i].replace(second=0).replace(tzinfo=None) in lastInterval:
+        for i in range(len(labelsAttack)):
+            if labelsAttack[i] == 1:
+                if sTimeAttack[i].replace(second=0).replace(tzinfo=None) in lastInterval and eTimeAttack[i].replace(second=0).replace(tzinfo=None) in lastInterval:
                     continue
-                elif sTime0[i].replace(second=0).replace(tzinfo=None) in lastInterval:
+                elif sTimeAttack[i].replace(second=0).replace(tzinfo=None) in lastInterval:
 
-                    nowInterval = pd.Interval(lastInterval.right, eTime0[i].replace(second=0).replace(tzinfo=None)+timedelta(minutes=1), closed="both")
-                    lastInterval = pd.Interval(lastInterval.left, eTime0[i].replace(second=0).replace(tzinfo=None) +timedelta(minutes=1), closed="both")
+                    nowInterval = pd.Interval(lastInterval.right, eTimeAttack[i].replace(second=0).replace(tzinfo=None)+timedelta(minutes=1), closed="both")
+                    lastInterval = pd.Interval(lastInterval.left, eTimeAttack[i].replace(second=0).replace(tzinfo=None) +timedelta(minutes=1), closed="both")
                 
-                elif eTime0[i].replace(second=0).replace(tzinfo=None) in lastInterval:
+                elif eTimeAttack[i].replace(second=0).replace(tzinfo=None) in lastInterval:
             
-                    nowInterval = pd.Interval(sTime0[i].replace(second=0).replace(tzinfo=None), lastInterval.left, closed="both")
-                    lastInterval = pd.Interval(sTime0[i].replace(second=0).replace(tzinfo=None), lastInterval.right, closed="both")
+                    nowInterval = pd.Interval(sTimeAttack[i].replace(second=0).replace(tzinfo=None), lastInterval.left, closed="both")
+                    lastInterval = pd.Interval(sTimeAttack[i].replace(second=0).replace(tzinfo=None), lastInterval.right, closed="both")
                 else:
-                    nowInterval = pd.Interval(sTime0[i].replace(second=0).replace(tzinfo=None), eTime0[i].replace(second=0).replace(tzinfo=None) +timedelta(minutes=1), closed="both")
+                    nowInterval = pd.Interval(sTimeAttack[i].replace(second=0).replace(tzinfo=None), eTimeAttack[i].replace(second=0).replace(tzinfo=None) +timedelta(minutes=1), closed="both")
                     lastInterval = nowInterval
 
                 axs[0].axvspan(nowInterval.left, nowInterval.right, facecolor="#2A9D8F")
-            sTimeCluster0.append(sTime0[i].replace(tzinfo=None))
-            packetsCluster0.append(packets0[i])
-        for i in range(len(labels1)):
-            if labels1[i] == 1:
-                if sTime1[i].replace(second=0).replace(tzinfo=None) in lastInterval and eTime1[i].replace(second=0).replace(tzinfo=None) in lastInterval:
+            sTimeClusterAttack.append(sTimeAttack[i].replace(tzinfo=None))
+            packetsClusterAttack.append(packetsAttack[i])
+            if packetsAttack[i] > maxValue:
+                maxValue = packetsAttack[i]
+        for i in range(len(labelsNormal)):
+            if labelsNormal[i] == 1:
+                if sTimeNormal[i].replace(second=0).replace(tzinfo=None) in lastInterval and eTimeNormal[i].replace(second=0).replace(tzinfo=None) in lastInterval:
                     continue
-                elif sTime1[i].replace(second=0).replace(tzinfo=None) in lastInterval:
+                elif sTimeNormal[i].replace(second=0).replace(tzinfo=None) in lastInterval:
 
-                    nowInterval = pd.Interval(lastInterval.right, eTime1[i].replace(second=0).replace(tzinfo=None)+timedelta(minutes=1), closed="both")
-                    lastInterval = pd.Interval(lastInterval.left, eTime1[i].replace(second=0).replace(tzinfo=None) +timedelta(minutes=1), closed="both")
+                    nowInterval = pd.Interval(lastInterval.right, eTimeNormal[i].replace(second=0).replace(tzinfo=None)+timedelta(minutes=1), closed="both")
+                    lastInterval = pd.Interval(lastInterval.left, eTimeNormal[i].replace(second=0).replace(tzinfo=None) +timedelta(minutes=1), closed="both")
                 
-                elif eTime1[i].replace(second=0).replace(tzinfo=None) in lastInterval:
+                elif sTimeNormal[i].replace(second=0).replace(tzinfo=None) in lastInterval:
             
-                    nowInterval = pd.Interval(sTime1[i].replace(second=0).replace(tzinfo=None), lastInterval.left, closed="both")
-                    lastInterval = pd.Interval(sTime1[i].replace(second=0).replace(tzinfo=None), lastInterval.right, closed="both")
+                    nowInterval = pd.Interval(sTimeNormal[i].replace(second=0).replace(tzinfo=None), lastInterval.left, closed="both")
+                    lastInterval = pd.Interval(sTimeNormal[i].replace(second=0).replace(tzinfo=None), lastInterval.right, closed="both")
                 else:
-                    nowInterval = pd.Interval(sTime1[i].replace(second=0).replace(tzinfo=None), eTime1[i].replace(second=0).replace(tzinfo=None) +timedelta(minutes=1), closed="both")
+                    nowInterval = pd.Interval(sTimeNormal[i].replace(second=0).replace(tzinfo=None), eTimeNormal[i].replace(second=0).replace(tzinfo=None) +timedelta(minutes=1), closed="both")
                     lastInterval = nowInterval
 
                 axs[1].axvspan(nowInterval.left, nowInterval.right, facecolor="#2A9D8F")
-            sTimeCluster1.append(sTime1[i].replace(tzinfo=None))
-            packetsCluster1.append(packets1[i])
+            sTimeClusterNormal.append(sTimeNormal[i].replace(tzinfo=None))
+            packetsClusterNormal.append(packetsNormal[i])
+
+            if packetsNormal[i] > maxValue:
+                maxValue = packetsNormal[i]
+
         startTime += clusterFrequency
 
-    axs[0].scatter(sTimeCluster0 ,packetsCluster0, color="#162931", label="Cluster 0")
+    axs[0].scatter(sTimeClusterAttack ,packetsClusterAttack, color="#162931", label="Attack cluster")
 
-    axs[1].scatter(sTimeCluster1 ,packetsCluster1, color="#E76F51", label="Cluster 1")
+    axs[1].scatter(sTimeClusterNormal ,packetsClusterNormal, color="#E76F51", label="Normal cluster")
 
     axs[0].xaxis.set(
         major_locator=mdates.MinuteLocator(interval=15),
         major_formatter=mdates.DateFormatter("%H:%M")
     )
-    axs[0].set_title("Packets in cluster 0")
+    axs[0].set_title("Packets in each cluster")
+    axs[0].title.set_size(20)
     axs[0].set_xlabel('Time')
     axs[0].set_ylabel("Packets")
-    axs[0].tick_params(axis='both', which='major', labelsize=12)
+    axs[0].set_ylim([0,maxValue])
+    axs[0].tick_params(axis='both', which='major', labelsize=17)
     axs[0].legend()
     #axs[0].text(0.7, 0.9, 'Labeled attack cluster: ' + str(deviation), horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, bbox=dict(facecolor='blue', alpha=0.2))
     
-    axs[1].set_title("Packets in each cluster 1")
     axs[1].xaxis.set(
         major_locator=mdates.MinuteLocator(interval=15),
         major_formatter=mdates.DateFormatter("%H:%M")
     )
     axs[1].set_xlabel('Time')
     axs[1].set_ylabel("Packets")
-    axs[1].tick_params(axis='both', which='major', labelsize=12)
+    axs[1].tick_params(axis='both', which='major', labelsize=17)
+    axs[1].set_ylim([0,maxValue])
     axs[1].legend()
     fig.tight_layout()
     fig.savefig("Plots/Kmeans/Attack0803/NetFlow/Combined/Packets.Scatter."+  str(systemId)+ "."+ str(int(interval.total_seconds())) +"secInterval.png", dpi=300)
     plt.close()
 
 
-systems = [ "trd-gw", "ifi2-gw5", 
-            "oslo-gw1"]
+systems = ["bergen-gw3"]
 startKmeans = "2023-03-08 14:15:00"
 stopKmeans= "2023-03-08 16:00:00"
 clusterFrequency = timedelta(minutes = 15)
