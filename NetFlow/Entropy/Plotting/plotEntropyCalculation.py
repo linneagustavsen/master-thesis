b1e620249f9d354938d7e030a1792ed0562da6c2
Linnea <linnea.gustavsen@gmail.com>
2023-05-21 11:41:55 +0200

    added plots from attack 2 and 3

--- NetFlow/Entropy/Plotting/plotEntropyCalculation.py
+++ NetFlow/Entropy/Plotting/plotEntropyCalculation.py
@@ -8,9 +8,11 @@ import pandas as pd
     Make a plot based on arrays of values and timestamps
 '''
 def makePlot(y_field, y_fieldName, systemId, interval, attackDate):
-    data = pd.read_csv("Calculations0803/Entropy/NetFlow/Metrics."+ str(int(interval.total_seconds())) +"secInterval.attack."+str(attackDate)+ "."+str(systemId)+ ".csv")
+    #data = pd.read_csv("Calculations0803/Entropy/NetFlow/Metrics."+ str(int(interval.total_seconds())) +"secInterval.attack."+str(attackDate)+ "."+str(systemId)+ ".csv")
+    #data = pd.read_csv("Calculations1703/Entropy/NetFlow/Metrics."+ str(int(interval.total_seconds())) +"secInterval.attack."+str(attackDate)+ "."+str(systemId)+ ".csv")
+    data = pd.read_csv("Calculations2403/Entropy/NetFlow/Metrics."+ str(int(interval.total_seconds())) +"secInterval.attack."+str(attackDate)+ "."+str(systemId)+ ".csv")
 
-    timeAxis = pd.to_datetime(data["eTime"])
+    timeAxis = pd.to_datetime(data["sTime"])
 
     y_values = data[y_field]
                       
@@ -18,16 +20,29 @@ def makePlot(y_field, y_fieldName, systemId, interval, attackDate):
    
     axs.plot(timeAxis ,y_values, color="#162931")
     format = '%b %d %H:%M:%S'
-    strings = [
+    '''strings = [
             ["Mar 08 14:29:55", "Mar 08 14:34:56"], ["Mar 08 14:49:56", "Mar 08 15:02:57"],
-            ["Mar 08 15:09:56", "Mar 08 15:17:02"], ["Mar 08 15:37:00", "Mar 08 15:52:02"]]
+            ["Mar 08 15:09:56", "Mar 08 15:17:02"], ["Mar 08 15:37:00", "Mar 08 15:52:02"]]'''
+    '''strings = [["Mar 17 11:00:01", "Mar 17 11:07:02"], ["Mar 17 11:37:02", "Mar 17 11:50:04"],
+           ["Mar 17 11:57:02", "Mar 17 12:04:12"], ["Mar 17 12:44:10", "Mar 17 13:00:17"]]'''
+    strings = [["Mar 24 14:00:01", "Mar 24 14:03:57"], ["Mar 24 14:13:29", "Mar 24 14:29:08"],
+           ["Mar 24 14:46:30", "Mar 24 14:55:00"], ["Mar 24 14:59:50", "Mar 24 15:15:06"], 
+           ["Mar 24 15:26:51", "Mar 24 15:39:22"], ["Mar 24 15:40:21", "Mar 24 15:47:50"], 
+           ["Mar 24 16:07:29", "Mar 24 16:19:00"], ["Mar 24 16:22:29", "Mar 24 16:29:13"],
+           ["Mar 24 16:29:53", "Mar 24 16:49:50"], ["Mar 24 16:53:22", "Mar 24 17:09:39"],
+           ["Mar 24 17:25:15", "Mar 24 17:47:00"]]
     for string in strings:
         start = datetime.strptime(string[0], format).replace(year=2023)
         stop = datetime.strptime(string[1], format).replace(year=2023)
         axs.axvspan(start, stop, facecolor="#F9CAA4")
     
     #format = '%Y-%m-%dT%H:%M:%SZ'
-    attackFlows = pd.read_csv("Calculations0803/Entropy/NetFlow/AttackFlows."+ str(int(interval.total_seconds())) +"secInterval.attack."+str(attackDate)+ "."+str(systemId)+ ".csv")
+    #attackFlows = pd.read_csv("Calculations0803/Entropy/NetFlow/AttackFlows."+ str(int(interval.total_seconds())) +"secInterval.attack."+str(attackDate)+ "."+str(systemId)+ ".csv")
+    #attackFlows = pd.read_csv("Calculations1703/Entropy/NetFlow/AttackFlows."+ str(int(interval.total_seconds())) +"secInterval.attack."+str(attackDate)+ "."+str(systemId)+ ".csv")
+    attackFlows = pd.read_csv("Calculations2403/Entropy/NetFlow/AttackFlows."+ str(int(interval.total_seconds())) +"secInterval.attack."+str(attackDate)+ "."+str(systemId)+ ".csv")
+    if len(attackFlows["eTime"])==0:
+        print("Empty")
+        return
     startTime = pd.to_datetime(attackFlows["sTime"])
     endTime = pd.to_datetime(attackFlows["eTime"])
     lastInterval = pd.Interval(pd.Timestamp.now().replace(tzinfo=None), pd.Timestamp.now().replace(tzinfo=None), closed="both")
@@ -58,13 +73,13 @@ def makePlot(y_field, y_fieldName, systemId, interval, attackDate):
     axs.set_ylabel(y_fieldName)
     axs.tick_params(axis='both', which='major', labelsize=12)
     fig.tight_layout()
-    fig.savefig("Plots/Entropy/Attack0803/NetFlow/Theoretical/"+  str(systemId)+ "." + str(y_field)+ "."+ str(int(interval.total_seconds())) +"secInterval.png", dpi=300)
+    fig.savefig("Plots/Entropy/Attack2403/NetFlow/"+  str(systemId)+ "." + str(y_field)+ "."+ str(int(interval.total_seconds())) +"secInterval.png", dpi=300)
     plt.close()
 
 
-systems = ["hovedbygget-gw", "trd-gw", "teknobyen-gw2", "teknobyen-gw1", "ifi2-gw5", 
-            "oslo-gw1", "tromso-gw5", "stangnes-gw", "rodbergvn-gw2", "narvik-kv-gw", "narvik-gw3", "tromso-fh-gw",
-            "ma2-gw", "narvik-gw4"]
+systems = ["tromso-gw5",  "teknobyen-gw1", "narvik-gw3", "hovedbygget-gw",
+         "teknobyen-gw2", "ma2-gw", "bergen-gw3", "narvik-kv-gw",  "trd-gw", "ifi2-gw5", 
+            "oslo-gw1"]
 y_fields = ["dstEntropy", "dstEntropyRate","srcEntropy", "srcEntropyRate", "flowEntropy", "flowEntropyRate", "numberOfFlows", "icmpRatio", 
             "icmpPackets", "packetSizeEntropy", "packetSizeEntropyRate", "numberOfPackets", "numberOfBytes"]
 y_field_names = ["Entropy of destination IP addresses", "Entropy rate of destination IP addresses", "Entropy of source IP addresses", "Entropy rate of source IP addresses", "Entropy of bi-directional flows", "Entropy rate of bi-directional flows", 
@@ -72,11 +87,11 @@ y_field_names = ["Entropy of destination IP addresses", "Entropy rate of destina
                  "Number of packets", "Number of bytes"]
 
 intervals = [timedelta(minutes = 5), timedelta(minutes = 10), timedelta(minutes = 15)]
-attackDate = "08.03.23"
-systemId = "teknobyen-gw1"
-
-for i in range(len(y_fields)):
-    for interval in intervals:
-        makePlot(y_fields[i], y_field_names[i], systemId, interval, attackDate)
-
-    break
+attackDate = "24.03.23"
+for systemId in systems:
+    print(systemId)
+    for i in range(len(y_fields)):
+        print(y_fields[i])
+        for interval in intervals:
+            print(interval)
+            makePlot(y_fields[i], y_field_names[i], systemId, interval, attackDate)
